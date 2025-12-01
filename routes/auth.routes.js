151fd1568e237cb485808deec124aaf4ce88b9fa
Ivan Kulinkovich <52656558+Cool2699@users.noreply.github.com>
2025-12-01 12:52:50 +0300

    Added get and update profile routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -80,4 +80,51 @@ router.post(
     }
 );
 
+router.get("/profile", async (req, res) => {
+    try {
+        const user = await User.findById(req.auth.id).select("-password");
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: req.t("userNotFound"),
+            });
+        }
+
+        res.json({
+            success: true,
+            data: user,
+        });
+    } catch (error) {
+        handleRouteError(res, error);
+    }
+});
+
+router.put("/profile", async (req, res) => {
+    try {
+        const userId = req.auth.id;
+        const requestBody = req.body;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: req.t("userNotFound"),
+            });
+        }
+
+        Object.keys(requestBody).forEach((key) => {
+            user[key] = requestBody[key];
+        });
+
+        await user.save();
+
+        res.json({
+            success: true,
+            message: req.t("profileUpdatedSuccessfully"),
+            data: user,
+        });
+    } catch (error) {
+        handleRouteError(res, error);
+    }
+});
+
 export default router;
